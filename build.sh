d02725aea4b2ba0f446085504b30ad0eded31a65
Ryan Pang <ryan.pang@live.com>
2022-09-05 12:48:04 +1200

    Clean up

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 &&
-  export PATH=$PATH:$JAVA_HOME/bin
-fi
-rm -rf node_modules package-lock.json target webpack.generated.js && pnpm install &&
+rm -rf target &&
 mvn install -Pdirectory
